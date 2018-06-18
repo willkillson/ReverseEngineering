4c56ed98e77205799b54ff9d5b2a4fa4b72c4c1f
kevin wilkinson <willkillson@gmail.com>
2018-06-17 17:49:01 -0700

    ReadIn comlete

--- ReadIn/ReadIn/Source.cpp
+++ ReadIn/ReadIn/Source.cpp
@@ -3,6 +3,24 @@
 
 int main() {
 
-	std::cout << "hello World!" << std::endl;
+	int intRead = 0;
+	HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, false, 13604);// https://msdn.microsoft.com/en-us/library/windows/desktop/ms684320(v=vs.85).aspx
+
+	if (handle == NULL) {
+		std::cout << "OpenProcess failed. Error code : " << GetLastError() << std::endl;
+		return 0;
+	}
+
+	LPCVOID lpcvBasePointer = (LPCVOID)0x00EFFB48;//LPCVOID is Long Pointer Const void
+	LPVOID lpvBufferPointer = &intRead;//LPVOID is Long Pointer Void
+
+
+	if (ReadProcessMemory(handle, lpcvBasePointer, lpvBufferPointer,sizeof(int),NULL) == NULL) {//https://msdn.microsoft.com/en-us/library/windows/desktop/ms680553(v=vs.85).aspx
+		std::cout << "ReadProcessMemory failed. Error code : " << GetLastError() << std::endl;
+		return 0;
+	}
+
+	std::cout << "intRead = " << intRead << std::endl;
+
 
 }
